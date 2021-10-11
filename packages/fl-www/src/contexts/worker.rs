13bc1db22e2be293010c364f0d492106a3a10911
Kaede Hoshikawa <futursolo@icloud.com>
2021-10-11 20:43:06 +0900

    Remove unused feature.

--- packages/fl-www/src/contexts/worker.rs
+++ packages/fl-www/src/contexts/worker.rs
@@ -8,6 +8,7 @@ pub(crate) fn worker_provider(props: &ChildrenProps) -> Html {
     let children = props.children.clone();
 
     use_state(|| agents::highlight::Worker::bridge(Callback::from(move |_| ())));
+    use_state(|| agents::markdown::Worker::bridge(Callback::from(move |_| ())));
 
     html! {
         <>
