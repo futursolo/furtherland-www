8c1475615353f6a333d3e2c3909b593d4bcd2972
Kaede Hoshikawa <futursolo@icloud.com>
2021-11-12 17:57:35 +0900

    Remove extra implementation.

--- packages/fl-www-common/src/prelude.rs
+++ packages/fl-www-common/src/prelude.rs
@@ -5,12 +5,12 @@ pub use std::borrow::Cow;
 pub use gloo;
 pub use log;
 // pub use reqwest;
+pub use gloo_utils::{document, window};
 pub use stylist;
 pub use stylist::YieldStyle;
 pub use wasm_bindgen::JsCast;
 pub use yew;
 pub use yew::prelude::*;
-pub use yew::utils::{document, window};
 
 pub use wasm_bindgen_futures::spawn_local;
 
@@ -18,9 +18,6 @@ pub use crate as common;
 // pub use fl_www_macros as macros;
 
 pub use crate::hooks;
-pub use crate::hooks::{
-    use_reducer, use_reducer_eq, use_state, use_state_eq, UseReducerHandle, UseStateHandle,
-};
 pub use crate::styling;
 
 pub use crate::browser::BrowserKind;
