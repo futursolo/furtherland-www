8c3bf20d49665b8e449b3afd5740f3801ee0a5b9
Kaede Hoshikawa <futursolo@icloud.com>
2022-03-05 12:00:19 +0900

    Persistent Token.

--- packages/fl-www/src/atoms/token.rs
+++ packages/fl-www/src/atoms/token.rs
@@ -1,6 +1,26 @@
-use bounce::prelude::*;
+use std::rc::Rc;
 
-#[derive(Atom, Default, PartialEq)]
+use bounce::prelude::*;
+use gloo::storage::{LocalStorage, Storage};
+use serde::{Deserialize, Serialize};
+use wasm_bindgen::prelude::*;
+
+static STORAGE_KEY: &str = "fl_resident_token";
+
+#[derive(Atom, PartialEq, Serialize, Deserialize)]
+#[observed]
 pub(crate) struct TokenState {
     pub inner: Option<String>,
+}
+
+impl Default for TokenState {
+    fn default() -> Self {
+        LocalStorage::get(STORAGE_KEY).unwrap_or(Self { inner: None })
+    }
+}
+
+impl Observed for TokenState {
+    fn changed(self: Rc<Self>) {
+        LocalStorage::set(STORAGE_KEY, &self).expect_throw("failed to set theme.");
+    }
 }
